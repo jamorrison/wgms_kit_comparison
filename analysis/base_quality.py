bf546dd42c6b395be1a682075b6880a156093f75
Jacob Morrison <Jacob.Morrison@vai.org>
2021-04-26 14:04:23 -0400

    Revision changes to raw read stats

--- analysis/base_quality.py
+++ analysis/base_quality.py
@@ -12,15 +12,27 @@ def base_metrics(phred_scores):
     no_bases = len(phred_scores)
 
     lo_count = 0
+    md_count = 0
+    hi_count = 0
     for score in phred_scores:
         if score < 20:
             lo_count += 1
+        elif (score >= 20) and (score <= 30):
+            md_count += 1
+        elif score > 30:
+            hi_count += 1
 
-    return avg_qual, no_bases, lo_count
+    return avg_qual, no_bases, lo_count, md_count, hi_count
 
 # Set up command line arguments
 parser = argparse.ArgumentParser(
     description = 'base_quality.py finds base quality metrics for input FASTQ files'
+)
+
+parser.add_argument(
+    '-c', '--check',
+    action = 'store_true',
+    help = 'Perform check on read names to ensure reads are ordered properly'
 )
 
 parser.add_argument(
@@ -46,7 +58,9 @@ r1_totrs = r2_totrs = 0 # Total number of reads processed
 r1_totbs = r2_totbs = 0 # Total number of bases processed
 r1_avg20 = r2_avg20 = 0 # Number of reads with avg. base quality >= 20
 r1_avg30 = r2_avg30 = 0 # Number of reads with avg. base quality >= 30
-r1_cnt20 = r2_cnt20 = 0 # Number of bases with base quality < 20
+r1_cntlo = r2_cntlo = 0 # Number of bases with base quality < 20
+r1_cntmd = r2_cntmd = 0 # Number of bases with base quality >= 20 and <= 30
+r1_cnthi = r2_cnthi = 0 # Number of bases with base quality > 30
 
 print('Begin processing {}'.format(args.fastq_1))
 t1_start = time.time()
@@ -54,7 +68,9 @@ with gzip.open(args.fastq_1, 'rt') as f1:
     for record in SeqIO.parse(f1, 'fastq'):
         r1_names.append(record.id)
 
-        avg, num, low = base_metrics(record.letter_annotations['phred_quality'])
+        avg, num, lo, md, hi = base_metrics(
+            record.letter_annotations['phred_quality']
+        )
 
         if avg >= 20:
             r1_avg20 += 1
@@ -63,9 +79,11 @@ with gzip.open(args.fastq_1, 'rt') as f1:
 
         r1_totrs += 1
         r1_totbs += num
-        r1_cnt20 += low
+        r1_cntlo += lo
+        r1_cntmd += md
+        r1_cnthi += hi
 t1_end = time.time()
-print('Processing {} took {:.2f} seconds'.format(args.fastq_1, t1_end-t1_start))
+print('Processing time = {:.2f} seconds'.format(t1_end-t1_start))
 
 print('Begin processing {}'.format(args.fastq_2))
 t2_start = time.time()
@@ -73,7 +91,9 @@ with gzip.open(args.fastq_2, 'rt') as f2:
     for record in SeqIO.parse(f2, 'fastq'):
         r2_names.append(record.id)
 
-        avg, num, low = base_metrics(record.letter_annotations['phred_quality'])
+        avg, num, lo, md, hi = base_metrics(
+            record.letter_annotations['phred_quality']
+        )
 
         if avg >= 20:
             r2_avg20 += 1
@@ -82,14 +102,20 @@ with gzip.open(args.fastq_2, 'rt') as f2:
 
         r2_totrs += 1
         r2_totbs += num
-        r2_cnt20 += low
+        r2_cntlo += lo
+        r2_cntmd += md
+        r2_cnthi += hi
 t2_end = time.time()
-print('Processing {} took {:.2f} seconds'.format(args.fastq_2, t2_end-t2_start))
+print('Processing time = {:.2f} seconds'.format(t2_end-t2_start))
 
-if r1_names == r2_names:
-    print('All reads are ordering correctly!')
-else:
-    print('There is a read out of order in:', args.fastq_1, 'and', args.fastq_2)
+if (args.check):
+    if r1_names == r2_names:
+        print('All reads are ordering correctly!')
+    else:
+        print(
+            'There is a read out of order in:',
+            args.fastq_1, 'and', args.fastq_2
+        )
 
 print(
     '''
@@ -99,13 +125,18 @@ print(
     Read 1 % of reads with avg. base quality >= 20: {:.2f}
     Read 1 % of reads with avg. base quality >= 30: {:.2f}
     Read 1 % of bases with base quality < 20: {:.2f}
-    '''.format(args.fastq_1,
-                r1_totrs,
-                r1_totbs,
-                100*r1_avg20/r1_totrs,
-                100*r1_avg30/r1_totrs,
-                100*r1_cnt20/r1_totbs
-                )
+    Read 1 % of bases with base quality >= 20 and <= 30: {:.2f}
+    Read 1 % of bases with base quality > 30: {:.2f}
+    '''.format(
+        args.fastq_1,
+        r1_totrs,
+        r1_totbs,
+        100*r1_avg20/r1_totrs,
+        100*r1_avg30/r1_totrs,
+        100*r1_cntlo/r1_totbs,
+        100*r1_cntmd/r1_totbs,
+        100*r1_cnthi/r1_totbs
+    )
 )
 
 print(
@@ -116,12 +147,17 @@ print(
     Read 2 % of reads with avg. base quality >= 20: {:.2f}
     Read 2 % of reads with avg. base quality >= 30: {:.2f}
     Read 2 % of bases with base quality < 20: {:.2f}
-    '''.format(args.fastq_2,
-                r2_totrs,
-                r2_totbs,
-                100*r2_avg20/r2_totrs,
-                100*r2_avg30/r2_totrs,
-                100*r2_cnt20/r2_totbs
-                )
+    Read 2 % of bases with base quality >= 20 and <= 30: {:.2f}
+    Read 2 % of bases with base quality > 30: {:.2f}
+    '''.format(
+        args.fastq_2,
+        r2_totrs,
+        r2_totbs,
+        100*r2_avg20/r2_totrs,
+        100*r2_avg30/r2_totrs,
+        100*r2_cntlo/r2_totbs,
+        100*r2_cntmd/r2_totbs,
+        100*r2_cnthi/r2_totbs
+    )
 )
 
